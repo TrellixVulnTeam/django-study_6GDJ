2b988f5ba1c233059b7c82f2dfe7ee769af3f343
helloworldff <34624289+helloworldff@users.noreply.github.com>
2019-05-28 23:10:40 +0800

    20190528提交

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,24 @@
 from django.shortcuts import render
 #from django.http import HttpResponse
-from blog.models import Post,Tag
+from blog.models import Post,Tag,Category
 
 # Create your views here.
 def post_list(request,category_id=None,tag_id=None):
+    tag = None
+    category = None
     if tag_id:
-        try:
-            tag = Tag.objects.get(id=tag_id)
-        except Tag.DoesNotExist:
-            post_list = []
-        else:
-            post_list = tag.post_set.filter(status=Post.STATUS_NORMAL)
+        post_list,tag = Post.get_by_tag(tag_id)
+    elif category_id:
+        post_list,category = Post.get_by_category(category_id)
     else:
-        post_list = Post.objects.filter(status=Post.STATUS_NORMAL)
-        if category_id:
-            post_list = post_list.filter(category_id=category_id)
+        post_list = Post.latest_posts()
 
-    return render(request,'blog/list.html',context={'post_list': post_list})
+    context = {
+        'category': category,
+        'tag': tag,
+        'post_list': post_list,
+    }
+    return render(request,'blog/list.html',context=context)
     #render(http的请求，模板名称，数据字典，页面编码类型，状态码，模板引擎解析)
     # content = 'post_list category_id={category_id},tag_id={tag_id}'.format(
     #     category_id = category_id,
