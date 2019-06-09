08d8b3060d026076107231723c99ec1119f460a7
helloworldff <34624289+helloworldff@users.noreply.github.com>
2019-06-09 23:55:33 +0800

    20190609 commit

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-#from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from blog.models import Post,Tag,Category
 from config.models import SideBar
-from django.views.generic import DetailView
+from django.views.generic import DetailView,ListView
 
 # Create your views here.
 def post_list(request,category_id=None,tag_id=None):
@@ -38,6 +38,68 @@ def post_detail(request,post_id=None):
     #return HttpResponse('detail')
     return render(request,'blog/detail.html',context={'post': post,'sidebars': SideBar.get_all()})
 
-class PostDetailView(DetailView):
-    model = Post
+class PostListView(ListView):
+    queryset = Post.latest_posts()
+    paginate_by = 1
+    context_object_name = 'post_list'
+    template_name = 'blog/list.html'
+
+class CommonViewMixin:
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'sidebars': SideBar.get_all(),
+        })
+        context.update(Category.get_navs())
+        return context
+
+class IndexView(CommonViewMixin,ListView):
+    model = Post    #定义客户端访问的模式，不然无法获取到值。
+    queryset = Post.latest_posts()
+    paginate_by = 5
+    context_object_name = 'post_list'
+    template_name = 'blog/list.html'
+
+    # def get_queryset(self):
+    #     queryset = Post.latest_posts()
+    #     return queryset
+
+class CategoryView(IndexView):
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        category_id = self.kwargs.get('category_id')
+        category = get_object_or_404(Category,pk=category_id)
+        context.update({
+            'category': category,
+        })
+        return context
+
+    def get_queryset(self):
+        """重写queryset,根据分类过滤"""
+        queryset = super().get_queryset()
+        category_id = self.kwargs.get('category_id')
+        return queryset.filter(category_id=category_id)
+
+class TagView(IndexView):
+    def get_context_data(self,**kwargs):
+        context = super.get_context_data(**kwargs)
+        tag_id = self.kwargs.get('tag_id')
+        tag = get_object_or_404(Tag,pk=tag_id)
+        context.update({
+            'tag': tag
+        })
+        return context
+
+    def get_queryset(self):
+        """重写queryset,根据标签过滤"""
+        queryset = super().get_queryset()
+        tag_id = self.kwargs.get('tag_id')
+        return queryset.filter(tag_id=tag_id)
+
+
+class PostDetailView(CommonViewMixin,DetailView):
+    model = Post     #定义客户端访问的模式，不然无法获取到值。
+    queryset = Post.latest_posts()
     template_name = 'blog/detail.html'
+    context_object_name = 'post'
+    pk_url_kwarg = 'post_id'
