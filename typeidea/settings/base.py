41b5ae09ff72b063bc0092e00fa1767b07e8d641
helloworldff <34624289+helloworldff@users.noreply.github.com>
2019-06-17 00:40:51 +0800

    20190617 commint

--- typeidea/settings/base.py
+++ typeidea/settings/base.py
@@ -54,11 +54,12 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'typeidea.urls'
+THEME = 'bootstrap'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, '../../templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'themes',THEME,'templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -127,3 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+#THEME = 'bootstrap'
+#STATIC_ROOT = 'tmp/static'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,"static"),
+
+]
